5c1da2ae2f82d6db26269e765e40bce73e72d611
kritbovorn <ktaweeyossak@yahoo.com>
2020-09-26 11:39:27 +0700

    Create Beginning Project

---
+++ Beginning/Beginning/BeginningApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeginningApp.swift
+//  Beginning
+//
+//  Created by Kritbovorn Taweeyossak on 26/9/2563 BE.
+//
+
+import SwiftUI
+
+@main
+struct BeginningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
