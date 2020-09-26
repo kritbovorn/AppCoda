4187bc71e136187fd190cf2f429273c49ff78aed
kritbovorn <ktaweeyossak@yahoo.com>
2020-09-26 13:40:38 +0700

    Create SwiftUIText Project

---
+++ SwiftUIText/SwiftUIText/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIText
+//
+//  Created by Kritbovorn Taweeyossak on 26/9/2563 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIText/SwiftUIText/SwiftUITextApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITextApp.swift
+//  SwiftUIText
+//
+//  Created by Kritbovorn Taweeyossak on 26/9/2563 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
